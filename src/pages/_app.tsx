8cc044db864d70dfdf12a56de101ecf76e2e9dd1
Doan Sy Hung <hungdoansy@gmail.com>
2022-07-20 22:05:20 +0700

    chore: erase to prepare for a better version

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { AppProps } from "next/app"
-import "@atlaskit/css-reset"
-
-const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-    return <Component {...pageProps} />
-}
-
-export default App
